e952f57c5995d48268cf44263a0ee2320399da30
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-03-10 14:32:25 +0530

    Fix render function

--- src/render.ts
+++ src/render.ts
@@ -1,11 +1,13 @@
 import Component from './component';
 import PrimaryComponent from './elements';
 import { init } from 'snabbdom/snabbdom';
+import { toVNode } from 'snabbdom/tovnode';
 import classModule from 'snabbdom/modules/class';
 import styleModule from 'snabbdom/modules/style';
 import eventModule from 'snabbdom/modules/eventlisteners';
 import attributeModule from 'snabbdom/modules/attributes';
 import propsModule from 'snabbdom/modules/props';
+import { log } from './utils';
 
 export const patch = init([
   classModule,
@@ -16,7 +18,9 @@ export const patch = init([
 ]);
 
 const render = (component: Component | PrimaryComponent, element: HTMLElement) => {
-  patch(element, component.render({ color: 'tomato' }, null));
+  const elementVNode = toVNode(element);
+  elementVNode.children = [component.render({ color: 'tomato' }, null)];
+  patch(element, elementVNode);
 }
 
 export { render as default, render }
