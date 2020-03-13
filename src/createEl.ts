c714e0438de46a28ba5e394b1a4b95d7a85051d5
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-03-13 18:25:38 +0530

    Fix to support JSX

--- src/createEl.ts
+++ src/createEl.ts
@@ -2,20 +2,23 @@ import { anyComp, log } from './utils';
 import { Context } from './context';
 import { PrimaryComponent, ChildType } from './elements';
 import { Component } from './component';
+import { elements } from './elements';
 
 interface protoClass {
   prototype: any;
 }
 
 export const createEl = (
-  component: anyComp | ((context: Context, props: Record<string, any>) => ChildType),
+  component: anyComp | ((context: Context, props: Record<string, any>) => ChildType) | string,
   props: Record<string, any>,
   ...children: (anyComp | ((context: Context) => ChildType))[]
 ) => {
   if (!props) props = {};
   props.children = children.flat(Infinity);
 
-  if (
+  if (elements[component as any]) {
+    return (elements[component as any] as any).create(props);
+  } else if (
     (component as protoClass).prototype instanceof PrimaryComponent ||
     (component as protoClass).prototype instanceof Component
   ) {
